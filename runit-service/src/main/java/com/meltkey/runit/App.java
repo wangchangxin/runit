0293e5fbaa9d43a58ab301077a29c3a656f2648e
wang changxin <wcx.email@gmail.com>
2013-03-26 13:51:13 +0800

    app init

---
+++ runit-service/src/main/java/com/meltkey/runit/App.java
@@ -0,0 +1,13 @@
+package com.meltkey.runit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
